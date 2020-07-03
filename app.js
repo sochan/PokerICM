f4f9fb68b0fa4f7fc18f16117b6812a9f5ffb913
SochanMao <smao@deakin.edu.au>
2020-07-04 08:58:54 +1000

    Done WhoWin function; next step is to analyse one input to form player1 and player2

--- app.js
+++ app.js
@@ -20,11 +20,85 @@ function OneHand(player1, player2){
 
 }
 
-// Who is the winner
+// who is the winner;
+// return: 0, 1, 2; 0-tie, 1-Player1 wins, 2-Player2 wins
 function WhoWin(player1, player2){
-    console.log("Test");
-    console.log(player1.name);
-    console.log(player2.name);   
+
+    // 10. Royal Flush
+    // Tie in RoyalFlush
+    if (player1.IsRoyalFlush && player2.IsRoyalFlush){
+        return 0;
+    } 
+    // Player 1 win at Royal Flush
+    if (player1.IsRoyalFlush)
+        return 1;
+    if (player2.IsRoyalFlush)
+        return 2;
+
+    // 9. StraightFlush
+    if (player1.StraightFlush > player2.StraightFlush)
+        return 1;
+    if (player2.StraightFlush > player1.StraightFlush)
+        return 2;
+
+    // 8. Four Kind
+    if (player1.FourKind > player2.FourKind)
+        return 1;
+    if (player2.FourKind > player1.FourKind)
+        return 2;
+
+    // 7. Full House; it should only test Three to see the winner but just follow the standard
+    if (player1.FullHouse.Three > player2.FullHouse.Three)
+        return 1;
+    if (player2.FullHouse.Three > player1.FullHouse.Three)
+        return 2;
+    if (player1.FullHouse.Pair > player2.FullHouse.Pair) // never reach but if the rules with more than 4 suits;
+        return 1;
+    if (player2.FullHouse.Pair > player1.FullHouse.Pair) // for example p1.three:333 and p2.three:333; 
+        return 2;
+    
+    // 6. Flush 
+    if (player1.Flush > player2.Flush)
+        return 1;
+    if (player2.Flush > player1.Flush)
+        return 2;
+    
+    // 5. Straight
+    if (player1.Straight > player2.Straight)
+        return 1;
+    if (player2.Straight > player1.Straight)
+        return 2;
+
+    // 4. Three kind
+    if (player1.ThreeKind > player2.ThreeKind)
+        return 1;
+    if (player2.ThreeKind > player1.ThreeKind)
+        return 2;
+
+    // 3. Two pairs
+    if (player1.TwoPairs.Pair1 > player2.TwoPairs.Pair1) // p1.Pair1 > p1.Pair2 during the assignment;
+        return 1;
+    if (player2.TwoPairs.Pair1 > player1.TwoPairs.Pair1) 
+        return 2;
+    if (player1.TwoPairs.Pair2> player2.TwoPairs.Pair2) // if Pair1 tie; now compare Pair2
+        return 1;
+    if (player2.TwoPairs.Pair2 > player1.TwoPairs.Pair2) 
+        return 2;
+
+    // 2. Pair
+    if (player1.Pair > player2.Pair)
+        return 1;
+    if (player2.Pair > player1.Pair)
+        return 2;
+    
+    // 1. High card
+    if (player1.HighCard > player2.HighCard)
+        return 1;
+    if (player2.HighCard > player1.HighCard)
+        return 2;
+
+    // by default no one is the winner; tie
+    return 0;
 }
 /// Testing and simulation
 var player1 = {
@@ -38,7 +112,7 @@ var player1 = {
     ThreeKind:0,
     TwoPairs:{Pair1:0, Pair2:0}, 
     Pair:0,
-    HighCard:0
+    HighCard:9
 };
 
 var player2 = {
@@ -52,11 +126,11 @@ var player2 = {
     ThreeKind:0,
     TwoPairs:{Pair1:0, Pair2:0}, 
     Pair:0,
-    HighCard:0
+    HighCard:9
 };
 
 // Who is the winner
-WhoWin(player1, player2);
+console.log(WhoWin(player1, player2));
 
 /// End Testing and simulation
 
