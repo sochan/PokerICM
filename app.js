e58cbc974bbb706862b5fae43584281ad21a0d54
SochanMao <smao@deakin.edu.au>
2020-07-05 11:49:11 +1000

    fix GetFourKind and done GetFullHouse

--- app.js
+++ app.js
@@ -121,21 +121,77 @@ function GetFourKind(arr_cards_oneplayer){
     for (let i=0; i < arr_cards_oneplayer.length; i++){
         card_values.push(arr_cards_oneplayer[i][0]);
     }
+
     // Sorting
     card_values.sort(function(a, b){return a - b});
     for(let i=0; i< card_values.length-1; i++){
         let itm1 = card_values[i];
         let itm2 = card_values[i+1];
+
         if (itm1 === itm2){
             if (!arr_four_k.includes(itm1))
                 arr_four_k.push(itm1);
             arr_four_k.push(itm2);
-        } 
+        } else break;
     }
-
     if (arr_four_k.length===4)
         return arr_four_k[0];
     return 0;
+}
+
+// GetFullHouse
+// Full house, Three of a kind and a Pair; 4C 4D 4S 5H 5D
+// Return: {Three, Pair}
+function GetFullHouse(arr_cards_oneplayer){
+    let full_house = {Three:0, Pair:0};
+    let arr1 =[]; // it might be a three or a pair
+    let arr2=[];
+    let card_values=[];
+    for (let i=0; i < arr_cards_oneplayer.length; i++){
+        card_values.push(arr_cards_oneplayer[i][0]);
+    }
+    // Sorting
+    card_values.sort(function(a, b){return a - b});
+
+    for (let i= 0; i< card_values.length -1; i++){
+        let itm1 = card_values[i];
+        let itm2 = card_values[i+1];
+        if (itm1 === itm2)
+        {
+            if (arr2.length>0){
+                if (!arr2.includes(itm1))
+                    arr2.push(itm1);
+                arr2.push(itm2);
+            } else{
+                if (!arr1.includes(itm1))
+                    arr1.push(itm1);
+                arr1.push(itm2);
+            }
+        } else if (arr2.length===0){
+            arr2.push(itm2);
+        }
+    }
+
+    if (arr1.length === 3 && arr2.length === 2)
+    {
+        full_house.Three = arr1[0];
+        full_house.Pair = arr2[0];
+    }
+
+    if (arr2.length === 3 && arr1.length === 2)
+    {
+        full_house.Three = arr2[0];
+        full_house.Pair = arr1[0];
+    }
+    
+    console.log(card_values);
+
+    console.log(arr1);
+    console.log(arr2);
+
+    console.log(full_house);
+
+    return full_house;
 }
 
 // One line from poker-hands or one hand; input: 9C 9D 8D 7C 3C 2S KD TH 9H 8H
@@ -172,17 +228,23 @@ function OneHand(oneLineCards){
     var arr = oneLineCards.split(" ");
     var arr1 = arr.slice(0, 5);
     var arr2 = arr.slice(5, 10);
-
+    // 10
     player1.IsRoyalFlush = GetIsRoyalFlush(arr1);
     player2.IsRoyalFlush = GetIsRoyalFlush(arr2);
 
+    // 9
     if (!player1.IsRoyalFlush)
         player1.StraightFlush = GetStraightFlush(arr1);
     if (!player2.IsRoyalFlush)
         player2.StraightFlush = GetStraightFlush(arr2);
 
+    // 8
     player1.FourKind = GetFourKind(arr1);
     player2.FourKind = GetFourKind(arr2);
+
+    // 7
+    player1.FullHouse = GetFullHouse(arr1);
+    player2.FullHouse = GetFullHouse(arr2);
     
     console.log(player1);
     console.log(player2);
@@ -303,7 +365,7 @@ var player2 = {
 // Who is the winner
 //console.log(WhoWin(player1, player2));
 
-OneHand("6C 8C 6H 6D 6S 9H 9D 9S 7C 9C");
+OneHand("6C 8C 6H 6D 6S 3H 9D 2S 2C 3D");
 
 /// End Testing and simulation
 
